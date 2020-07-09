5ba05a994830bd8e569cdac26ea4ce0e5ad91299
RajarsiGit <rajarsi3997@gmail.com>
2020-07-09 21:21:58 +0530

    commit

--- app/main.py
+++ app/main.py
@@ -5,8 +5,17 @@ from chatterbot.trainers import ListTrainer
 app = Flask(__name__)
  
 bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
-trainer = ChatterBotCorpusTrainer(bot)
-trainer.train("chatterbot.corpus.english")
+trainer = ListTrainer(bot)
+
+trainer.train([
+    "Hi there!",
+    "Hello",
+])
+
+trainer.train([
+    "Greetings!",
+    "Hello",
+])
 
 @app.route("/")
 def home():
