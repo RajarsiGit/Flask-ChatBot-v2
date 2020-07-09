459e7914b575c026c85b82f0f792f86a27644acc
RajarsiGit <rajarsi3997@gmail.com>
2020-07-09 21:29:31 +0530

    commit

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
@@ -26,29 +25,3 @@ def home():
 def get_bot_response():
     userText = request.args.get('msg')
     return str(bot.get_response(userText))
-=======
-from flask import Flask, render_template, request
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
-
-app = Flask(__name__)
- 
-bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
-trainer = ListTrainer(bot)
-trainer.train([
-	"How are you?",
-	"I am good.",
-	"That is good to hear.",
-	"Thank you",
-	"You are welcome.",
-])
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    return str(bot.get_response(userText))
->>>>>>> 2fac487336aa3f004beb17e605dc65ea8cb5ad86
--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 from app.main import app
 
 if __name__ == "__main__":
-=======
-from app.main import app
-
-if __name__ == "__main__":
->>>>>>> 2fac487336aa3f004beb17e605dc65ea8cb5ad86
 	app.run()
