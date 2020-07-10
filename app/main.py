dabb51f197dd0e7e102f79915405e5055b609404
RajarsiGit <rajarsi3997@gmail.com>
2020-07-10 09:58:22 +0530

    commit

--- app/main.py
+++ app/main.py
@@ -4,29 +4,22 @@ from chatterbot.trainers import ListTrainer
 
 app = Flask(__name__)
  
-bot = ChatBot(
-    'Flask ChatBot V2',
-    storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    logic_adapters=[
-        {
-            'import_path': 'chatterbot.logic.BestMatch'
-        },
-        {
-            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
-            'threshold': 0.90,
-            'default_response': 'I am sorry, but I do not understand.'
-        }
-    ]
-)
-
+bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 trainer = ListTrainer(bot)
 
 trainer.train([
-    'How can I help you?',
-    'I want to create a chat bot',
-    'Have you read the documentation?',
-    'No, I have not',
-    'This should help get you started: http://chatterbot.rtfd.org/en/latest/quickstart.html'
+    "Hi there!",
+    "Hello",
+])
+
+trainer.train([
+    "Greetings!",
+    "Hello",
+])
+
+trainer.train([
+    "What's your name?",
+    "I'm ChatBot v2.0",
 ])
 
 @app.route("/")
